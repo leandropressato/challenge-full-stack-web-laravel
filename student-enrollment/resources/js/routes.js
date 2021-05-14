7f301f67435f2a577c6e24658d891c98fb98010d
unknown <leandrocp14@hotmail.com>
2021-05-13 22:05:45 -0300

    create vue crud routes

---
+++ student-enrollment/resources/js/routes.js
@@ -0,0 +1,21 @@
+import AllStudent from './components/AllStudent.vue';
+import CreateStudent from './components/CreateStudent.vue';
+import EditStudent from './components/EditStudent.vue';
+ 
+export const routes = [
+    {
+        name: 'home',
+        path: '/',
+        component: AllStudent
+    },
+    {
+        name: 'create',
+        path: '/create',
+        component: CreateStudent
+    },
+    {
+        name: 'edit',
+        path: '/edit/:id',
+        component: EditStudent
+    }
+];
